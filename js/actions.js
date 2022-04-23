06c29903ad3370a365a3bc292d70c717221656f7
Sara-Mlh <sara.lyna26@yahoo.fr>
2022-04-24 00:28:43 +0100

    design fest web site

--- js/actions.js
+++ js/actions.js
@@ -1,6 +1,7 @@
 document.getElementById('.button').addEventListener('click',function(){
     document.querySelector('.registrations-modal').style.display='flex';
 });
+
 document.querySelector('.close').addEventListener('click',function(){
     document.querySelector('.registrations-modal').style.display='none';
 });
